9de86aef1e69ec044a58593704d448a405e5fdd1
FranciscoVergaraC <franciscovergaracr@gmail.com>
2022-05-20 08:52:48 -0300

    Manufacturing logisctics and medicin upg

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let login = document.getElementById('LoginButton');
 login.onclick = function () {
     window.location.assign("../Modulo Login/login.html");
 }
+
+let homeimg = document.getElementById('image-container')
+
+homeimg.onclick = function () {
+    window.location.assign("./index.html");
+}

05e94c1514f7e0ff1dc9f128d24868476b1a2f41
FranciscoVergaraC <franciscovergaracr@gmail.com>
2022-06-02 13:14:44 -0300

    About us added

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ let homeimg = document.getElementById('image-container')
 homeimg.onclick = function () {
     window.location.assign("./index.html");
 }
+
+let aboutus = document.getElementById('aboutus');
+
+aboutus.onclick = function () {
+    window.location.assign("./aboutus.html");
+}

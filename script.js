38c233fd5b20e1fa50988a2ca72ca5179f058920
FranciscoVergaraC <franciscovergaracr@gmail.com>
2022-06-04 19:06:27 -0300

    about us added

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let login = document.getElementById('LoginButton');
+let fvc = document.getElementById('fvc');
+let aboutus = document.getElementById('aboutus');
+let homeimg = document.getElementById('image-container')
 
 login.onclick = function () {
     window.location.assign("../Modulo Login/login.html");
 }
 
-let homeimg = document.getElementById('image-container')
-
 homeimg.onclick = function () {
     window.location.assign("./index.html");
 }
 
-let aboutus = document.getElementById('aboutus');
-
 aboutus.onclick = function () {
     window.location.assign("./aboutus.html");
+}
+
+fvc.onclick = function () {
+    window.location.assign("https://www.linkedin.com/in/francisco-vergara-cruz-48425743/");
 }
